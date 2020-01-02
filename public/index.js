27b06a6e4a80f72d6565cc4ad51a1defead8d819
blaeugh <50417843+blaeugh@users.noreply.github.com>
2020-01-02 11:45:09 -0500

    new files

--- public/index.js
+++ public/index.js
@@ -1,14 +1,48 @@
 const path = require("path");
+const hbs = require("hbs");
 const express = require("express");
 const app = express();
 
-app.get("", (req, res) =>{
-    res.send("thami shits himself regularly, and because of this he smells")
-})
-app.get("/piss", (req, res) =>{
-    res.send("thami also pisses himself regularly, which contributes to his odor")
-})
+//define paths for express
+const publicDirectoryPath = path.join(__dirname, "../public");
+const partialsPath = path.join(__dirname, "../templates/partials");
+const viewsPath = path.join(__dirname, "../templates/views");
+//setup handlebars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath); //telling express to get templates from templates/views folder
+hbs.registerPartials(partialsPath);
+//set up our static asset directory (gimme dat CSS)
+app.use(express.static(publicDirectoryPath));
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Our First Express App"
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+app.get("/about/:id", async (req, res) => {
+  const name = req.params.id;
+  try {
+    res.render("index", {
+      title: name
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+app.get("/anything", async (req, res) => {
+  try {
+    res.render("swag");
+  } catch {
+    res.status(500).send();
+  }
+});
 
 app.listen(3000, () => {
-    console.log('listening on port 3000')
+  console.log("Listening on port 3000");
+  console.log(viewsPath);
 });
+
+//127.0.0.1:3000
